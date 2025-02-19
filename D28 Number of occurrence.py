4e82e7dd18635dc17860d1389721c6c60387b984
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2025-02-19 19:52:30 +0530

    Create D28 Number of occurrence.py

---
+++ D28 Number of occurrence.py
@@ -0,0 +1,16 @@
+'''
+Given a sorted array, arr[] and a number target, you need to find the number of occurrences of target in arr[]. 
+
+Examples :
+
+Input: arr[] = [1, 1, 2, 2, 2, 2, 3], target = 2
+Output: 4
+Explanation: target = 2 occurs 4 times in the given array so the output is 4.
+'''
+Given a sorted array, arr[] and a number target, you need to find the number of occurrences of target in arr[]. 
+
+Examples :
+
+Input: arr[] = [1, 1, 2, 2, 2, 2, 3], target = 2
+Output: 4
+Explanation: target = 2 occurs 4 times in the given array so the output is 4.
