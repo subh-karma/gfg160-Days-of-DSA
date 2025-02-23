4102dcbf7fb97e2dc023537b204d4b68d5fcb69d
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2025-02-23 18:57:29 +0530

    Create D32 K-th element of two Arrays.py

---
+++ D32 K-th element of two Arrays.py
@@ -0,0 +1,16 @@
+'''
+Given two sorted arrays a[] and b[] and an element k, the task is to find the element that would be at the kth position of the combined sorted array.
+
+Examples :
+
+Input: a[] = [2, 3, 6, 7, 9], b[] = [1, 4, 8, 10], k = 5
+Output: 6
+Explanation: The final combined sorted array would be [1, 2, 3, 4, 6, 7, 8, 9, 10]. The 5th element of this array is 6.
+'''
+
+class Solution:
+
+    def kthElement(self, a, b, k):
+        a.extend(b[:])
+        a.sort()
+        return a[k-1]
