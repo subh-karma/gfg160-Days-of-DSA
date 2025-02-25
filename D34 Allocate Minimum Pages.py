bb65a6a48dcb57cf430934be1c10b3b3601ca19e
Subhasish Karmakar <75238941+subh-karma@users.noreply.github.com>
2025-02-25 18:32:30 +0530

    Create D34 Allocate Minimum Pages.py

---
+++ D34 Allocate Minimum Pages.py
@@ -0,0 +1,38 @@
+'''
+ou are given an array arr[] of integers, where each element arr[i] represents the number of pages in the ith book. You also have an integer k representing the number of students. The task is to allocate books to each student such that:
+
+Each student receives atleast one book.
+Each student is assigned a contiguous sequence of books.
+No book is assigned to more than one student.
+The objective is to minimize the maximum number of pages assigned to any student. In other words, out of all possible allocations, find the arrangement where the student who receives the most pages still has the smallest possible maximum.
+
+Note: Return -1 if a valid assignment is not possible, and allotment should be in contiguous order (see the explanation for better understanding).
+
+Examples:
+
+Input: arr[] = [12, 34, 67, 90], k = 2
+Output: 113
+Explanation: Allocation can be done in following ways:
+[12] and [34, 67, 90] Maximum Pages = 191
+[12, 34] and [67, 90] Maximum Pages = 157
+[12, 34, 67] and [90] Maximum Pages = 113.
+Therefore, the minimum of these cases is 113, which is selected as the output.
+'''
+ou are given an array arr[] of integers, where each element arr[i] represents the number of pages in the ith book. You also have an integer k representing the number of students. The task is to allocate books to each student such that:
+
+Each student receives atleast one book.
+Each student is assigned a contiguous sequence of books.
+No book is assigned to more than one student.
+The objective is to minimize the maximum number of pages assigned to any student. In other words, out of all possible allocations, find the arrangement where the student who receives the most pages still has the smallest possible maximum.
+
+Note: Return -1 if a valid assignment is not possible, and allotment should be in contiguous order (see the explanation for better understanding).
+
+Examples:
+
+Input: arr[] = [12, 34, 67, 90], k = 2
+Output: 113
+Explanation: Allocation can be done in following ways:
+[12] and [34, 67, 90] Maximum Pages = 191
+[12, 34] and [67, 90] Maximum Pages = 157
+[12, 34, 67] and [90] Maximum Pages = 113.
+Therefore, the minimum of these cases is 113, which is selected as the output.
